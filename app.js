f3408ac6a9221e2fcc3e17317246cc611e3d04cd
Jack Hanford <jackhanford@gmail.com>
2014-10-30 07:50:42 -0700

    Rewriting most

--- app.js
+++ app.js
@@ -1,128 +1,76 @@
 var fs = require('fs');
+var moment = require('moment');
+var he = require('he');
 var sentiment = require('sentiment');
 var HTMLParser = require('fast-html-parser');
 
-fs.readFile('./facebook/html/messages.htm', function(err, content) {
+var messageCount = {};
+
+fs.readFile('./facebook/html/messages.htm', 'utf8', function(err, content) {
   if (err) throw err;
   var root = HTMLParser.parse(content.toString());
   var user = root.querySelectorAll('.user');
   var message = root.querySelectorAll('p');
   var time = root.querySelectorAll('.meta');
 
-  var bringTogether = function (dict, options) {
-    var newArr = [];
 
-    for (var item in dict)
-      newArr.push([item, {occur: dict[item]}])
+  var stream = fs.createWriteStream('output.csv');
+  stream.write('userName, timestamp' + '\n')
 
-    if (options) {
-      if (options.sentiment) {
-        newArr = newArr.map(function(item) {
-          var phrase = item[0];
-          var val = item[1];
-          val.rating = sentiment(phrase);
-
-          return [phrase, val];
-        });
-      }
-    }
-
-    newArr.sort(function(a, b) {
-
-      if (options && options.times) {
-        return a[1].occur.count - b[1].occur.count;
-      } else {
-        return a[1].occur - b[1].occur;
-      }
-    })
-
-    return newArr.filter(function(item) {
-      if (options && options.times) {
-        return item[1].occur.count > 1
-      } else {
-        return item[1].occur > 2
-      }
-    });
-  }
-
-  var name = {};
-  var word = {};
-  var date = {};
-  var phrase = {};
-  var times = {};
-
-  var dayCount = {};
-
-  var namesList = [];
-  var wordList = [];
-  var phraseList = [];
+  var calendar = {};
 
   message.map(function(element, idx) {
+    console.log(idx + ' messages');
     if (element.childNodes.length == 0) return; 
     var timestamp = time[idx].childNodes[0].rawText;
-    var userName = user[idx].childNodes[0].rawText;
+    var userName = he.decode(user[idx].childNodes[0].rawText);
+    var message = element.childNodes[0].rawText;
 
     if (userName === 'Jack Hanford') {
       return;
     }
 
-    if (!times[timestamp]) {
-      times[timestamp] = {
-        count: 0
-      };
+    // Monday, September 10, 2012 at 10:51pm PDT - Timestamp pre moment
+    var stamp = moment(timestamp, ['MMM DD, YYYY at HH:mmA']);
+    var stampYear = stamp.year();
+    var stampMonth = stamp.month();
+    var stampDay = stamp.day();
+
+    // if (!(stampYear > startYear && stampYear < endYear)) {
+    //   return;
+    // }
+
+    if (!calendar[stampYear]) {
+      calendar[stampYear] = {};
     }
 
-    if (!times[timestamp][userName]) {
-      times[timestamp][userName] = {
-        messages: 0
-      };
+    if (!calendar[stampYear][stampMonth]) {
+      calendar[stampYear][stampMonth] = {};
     }
 
-    times[timestamp].count++;
-    times[timestamp][userName].messages++;
-
-    if (element.childNodes && element.childNodes[0].rawText) {
-      var text = element.childNodes.pop().rawText.toLowerCase();
-      var sentimentRating = sentiment(text);
-      var charCheck = text.split(' ');
-      if (charCheck.length > 1){
-        phrase[text] ? phrase[text] ++ : phrase[text] = 1;
-      }
-
-      var eachWordArray = text.split(' ');
-      eachWordArray.map(function(item) {
-        word[item] ? word[item] ++ : word[item] = 1;
-      })
+    if (!calendar[stampYear][stampMonth][stampDay]) {
+      calendar[stampYear][stampMonth][stampDay] = {};
     }
 
-    console.log(idx + ' out of ' + message.length + ' messages');
-  })
+    if (!calendar[stampYear][stampMonth][stampDay][userName]) {
+      calendar[stampYear][stampMonth][stampDay][userName] = [];
+    }
 
-  user.map(function(element) {
-    var currName = element.childNodes.pop().rawText;
-    name[currName] ? name[currName] ++ : name[currName] = 1;
-  })
+    if (!messageCount[userName]) {
+      messageCount[userName] = [];
+    }
 
-  phraseList = bringTogether(phrase, {sentiment: true});
-  wordList = bringTogether(word);
-  namesList = bringTogether(name);
-  timesList = bringTogether(times, {times: true});
+    messageCount[userName].push(timestamp);
+    calendar[stampYear][stampMonth][stampDay][userName].push(timestamp);
+    // message
 
-  timesList.map(function(element) {
-    var frequency = 0;
-    var day = element[0].split(',')[0];
-
-    dayCount[day] ? dayCount[day] ++ : dayCount[day] = 1;
+    stream.write(userName + ',' + timestamp + '\n');
   });
 
-  // var data = JSON.stringify({ 'Phrases': phraseList, 'Words': wordList, 'Names': namesList }, null, 2);
-  var data = JSON.stringify({
-    'Words': wordList,
-    'Phrases': phraseList,
-    'Names': namesList,
-    'Timestamps': timesList,
-    'Days': dayCount
-  })
+  stream.end();
+
+  
+  var data = JSON.stringify(calendar )
   fs.writeFile('data.json', data, function (err) {
     if (err) throw err;
     console.log('It\'s saved!');
--- assets/js/app.js
+++ assets/js/app.js
@@ -1,62 +1,10 @@
 var app = angular.module('fbDataApp', ['tc.chartjs']);
 
-app.controller('fbDataCtrl', ['$scope', '$timeout', '$http', function($scope, $timeout, $http){
-  var d;
+app.controller('fbDataCtrl', ['$scope', '$timeout', '$http', function($scope, $timeout, $http) {
   $http.get('../../data.json').success(function(response) {
-    day = response.Days;
-    $scope.words = response.Words.reverse();
-    $scope.phrases = response.Phrases;
-    console.log(response)
-    $scope.data = {
-      labels: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'],
-      datasets: [
-      {
-        label: 'Most active messenger days',
-        fillColor: 'rgba(151,187,205,0.2)',
-        strokeColor: 'rgba(151,187,205,1)',
-        pointColor: 'rgba(151,187,205,1)',
-        pointStrokeColor: '#fff',
-        pointHighlightFill: '#fff',
-        pointHighlightStroke: 'rgba(151,187,205,1)',
-        data: [day['Monday'], day['Tuesday'], day['Wednesday'], day['Thursday'], day['Friday'], day['Saturday'], day['Sunday']]
-      }
-      ]
-    };
-
-    // Chart.js Options
-    $scope.options =  {
-      // Sets the chart to be responsive
-      responsive: true,
-      ///Boolean - Whether grid lines are shown across the chart
-      scaleShowGridLines : true,
-      //String - Colour of the grid lines
-      scaleGridLineColor : "rgba(0,0,0,.05)",
-      //Number - Width of the grid lines
-      scaleGridLineWidth : 1,
-      //Boolean - Whether the line is curved between points
-      bezierCurve : true,
-      //Number - Tension of the bezier curve between points
-      bezierCurveTension : 0.4,
-      //Boolean - Whether to show a dot for each point
-      pointDot : true,
-      //Number - Radius of each point dot in pixels
-      pointDotRadius : 4,
-      //Number - Pixel width of point dot stroke
-      pointDotStrokeWidth : 1,
-      //Number - amount extra to add to the radius to cater for hit detection outside the drawn point
-      pointHitDetectionRadius : 20,
-      //Boolean - Whether to show a stroke for datasets
-      datasetStroke : true,
-      //Number - Pixel width of dataset stroke
-      datasetStrokeWidth : 2,
-      //Boolean - Whether to fill the dataset with a colour
-      datasetFill : true,
-      onAnimationProgress: function(){},
-      onAnimationComplete: function(){},
-      legendTemplate : '<ul class="tc-chart-js-legend"><% for (var i=0; i<datasets.length; i++){%><li><span style="background-color:<%=datasets[i].strokeColor%>"></span><%if(datasets[i].label){%><%=datasets[i].label%><%}%></li><%}%></ul>'
-    };
+    $scope.items = response;
+    console.log(Object.keys(response).length, response)
+  }).error(function(err) {
+    console.log(err)
   })
-
-
-
 }]);
