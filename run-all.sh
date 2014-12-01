12e36eca6573b4f3c5e517f14b4dab4e9bfea653
Jack Hanford <jackhanford@gmail.com>
2014-11-30 17:31:29 -0800

    Openning browser from terminal now

--- run-all.sh
+++ run-all.sh
@@ -12,13 +12,12 @@ sleep 3
 echo "Moving facebook.zip"
 mv facebook.zip ../
 cd ../
-echo "Grabbing node modules.." 
+echo "Grabbing node modules.."
 npm install
 sleep 3
-echo "Starting Node Script" 
+echo "Starting Node Script"
 node app
-echo "Getting Bower packages" 
+echo "Getting Bower packages"
 bower i
-echo "Starting Server" 
-echo "Go to localhost:8000!" 
-python -m SimpleHTTPServer
+echo "Starting Server"
+open "http://localhost:8000" & python -m SimpleHTTPServer
